2e7a1c54758ddeb73f7aa2ff2a1bffc1bc11d9b5
carmel-oz <92310226+carmel-oz@users.noreply.github.com>
2026-07-01 14:06:45 +0300

    Support GitHub Pages fallback

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig: NextConfig = {
+  ...(isGitHubPages
+    ? {
+        assetPrefix: "/package-collector/",
+        basePath: "/package-collector",
+      }
+    : {}),
   output: "export",
   reactStrictMode: true,
   images: {
